ee0435f412b5824cad06faf938107e5cd3d57863
Alexander Bobylev <alexander.bobylev@gmail.com>
2022-04-25 19:02:32 +0300

    Add new buttons

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, executor, types
 import requests
 import logging
 import configparser
-from datetime import date
+from datetime import date, timedelta
 
 logging.basicConfig(level=logging.INFO)
 
@@ -29,7 +29,7 @@ async def start_handler(message: types.Message):
         star = ''
         if item['cc'] in ('USD', 'EUR'):
             star = '⭐ '
-        buttons.append(types.InlineKeyboardButton(text=star + item['cc'], callback_data='btn_' + item['cc']))
+        buttons.append(types.InlineKeyboardButton(text=star + item['cc'], callback_data='btn_list_' + item['cc']))
 
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(*buttons)
@@ -40,22 +40,40 @@ async def start_handler(message: types.Message):
         reply_markup=keyboard
     )
 
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('btn_'))
+@dp.callback_query_handler(lambda c: c.data and c.data.startswith('btn_list_'))
 async def callback_query_handler(callback: types.CallbackQuery):
     cc = callback.data[-3:]
+    buttons = []
 
     if cc == 'RUB':
         msg = f"🚢🔥 русский военный корабль, иди на хуй\n🇺🇦 СЛАВА УКРАЇНІ!!!\n🇺🇦 ГЕРОЯМ СЛАВА!!!"
     else:
         today = date.today().strftime("%Y%m%d")
-        response = requests.get(NBU_CURRENCY_URL.format(code = cc, date = today)).json()
-        msg = f"{response[0]['exchangedate']}\n{response[0]['txt']} ({response[0]['cc']}): {response[0]['rate']} грн."
+        response_today = requests.get(NBU_CURRENCY_URL.format(code = cc, date = today)).json()
 
-    await callback.answer(
-        text=msg,
-        show_alert=True
+        yesterday = (date.today() - timedelta(days=1)).strftime("%Y%m%d")
+        response_yesterday = requests.get(NBU_CURRENCY_URL.format(code = cc, date = yesterday)).json()
+
+        delta = response_today[0]['rate'] - response_yesterday[0]['rate']
+        direct = ""
+        if (delta > 0):
+            direct = "({:.4f}".format(delta) + " грн.) 🔺"
+        elif (delta < 0):
+            direct = "({:.4f}".format(delta) + " грн.) 🔻"
+
+        msg = f"{response_today[0]['exchangedate']}\n{response_today[0]['txt']} ({response_today[0]['cc']}): {response_today[0]['rate']} грн. {direct}"
+        buttons.append(types.InlineKeyboardButton(text=f"Конвертувати грн в {cc}", callback_data='btn_conv_in_' + cc))
+        buttons.append(types.InlineKeyboardButton(text=f"Конвертувати {cc} в грн", callback_data='btn_conv_out_' + cc))
+        buttons.append(types.InlineKeyboardButton(text=f"Динаміка за 7 днів", callback_data='btn_graph_7_' + cc))
+
+    keyboard = types.InlineKeyboardMarkup(1)
+    keyboard.add(*buttons)
+
+    await callback.message.answer(
+        msg,
+        parse_mode=types.ParseMode.HTML,
+        reply_markup=keyboard
     )
-
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
